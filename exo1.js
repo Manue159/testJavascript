17a4058eaee0158d3cdc6dd80e0f84ea3bef546d
Manue159 <manuegn145@gmail.com>
2023-04-20 15:45:16 +0100

    files updated

--- exo1.js
+++ exo1.js
@@ -1,11 +1,11 @@
 function includes(document, tagName, text) {
     const elements = document.getElementsByTagName(tagName);
     for (let i = 0; i < elements.length; i++) {
-        if (elements[i].textContent === text) {
-            return true;
+        if (elements[i].textContent.includes(text)) {
+            console.log(true);
         }
     }
-    return false;
+    console.log(false);
 }
 
 includes(document.body, "p", "welcome");
